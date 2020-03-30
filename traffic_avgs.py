7589825e096ef44e87940099eae720b419fe4570
Jake Adams <jdadams@utah.gov>
2020-03-30 17:35:09 -0600

    arcpy scratch rather than tempfile

--- traffic_avgs.py
+++ traffic_avgs.py
@@ -3,9 +3,7 @@ import pandas as pd
 import numpy as np
 import json
 import os
-import tempfile
 import arcgis
-import getpass
 
 import secrets
 
@@ -36,76 +34,85 @@ for i in station_ids:
 #: Transpose so that the index becomes the keys and the rows are the values
 avgs_dict = avgs_df.T.to_dict()
 
-#: Load features into a structured numpy array
+#: Load features into a feature set
 feature_set = arcpy.FeatureSet()
 feature_set.load(secrets.FEATURE_URL)
-# feature_array = arcpy.da.FeatureClassToNumPyArray(feature_set, '*')
 
-#: Save features to .json, load .json as a feature class, insert new info via insertcursor
-with tempfile.TemporaryDirectory() as temp_dir:
-    temp_json_path = os.path.join(temp_dir, 'features.json')
-    print(f'Saving JSON to {temp_json_path}...')
-    with open(temp_json_path, 'w') as json_file:
-        json_file.write(feature_set.JSON)
+feature_name = 'TrafficChanges'
 
-    print(f'Creating temp gdb...')
-    arcpy.CreateFileGDB_management(temp_dir, 'temp.gdb')
-    temp_fc_path = os.path.join(temp_dir, 'temp.gdb', 'features')
-    print(f'Creating temp feature class {temp_fc_path}...')
-    arcpy.JSONToFeatures_conversion(temp_json_path, temp_fc_path)
+temp_json_path = os.path.join(arcpy.env.scratchFolder, 'features.json')
+temp_fc_path = os.path.join(arcpy.env.scratchGDB, 'features')
+sddraft_path = os.path.join(arcpy.env.scratchFolder, f'{feature_name}.sddraft')
+sd_path = sddraft_path[:-5]
 
-    #: Add our new columns.
-    print('Adding columns...')
-    columns = [('DetectorStation', 'TEXT'), ('AvgChange7D', 'DOUBLE'), ('StartDate', 'TEXT'), ('EndDate', 'TEXT')]
-    for col in columns:
-        name, dtype = col
-        arcpy.AddField_management(temp_fc_path, name, dtype)
+#: Make sure none of our files already exist
+paths = [sddraft_path, sd_path, temp_json_path, temp_fc_path]
+for item in paths:
+    if arcpy.Exists(item):
+        arcpy.Delete_management(item)
 
-    #: Update the temp feature class with new averages
-    print('Updating feature class with new averages...')
-    fields = ['DetectorStation', 'AvgChange7D', 'StartDate', 'EndDate']
-    with arcpy.da.UpdateCursor(temp_fc_path, fields) as ucursor:
-        for row in ucursor:
-            station = row[0]
-            if station in avgs_dict:
-                row[1] = avgs_dict[station]['AvgChange7D']
-                row[2] = avgs_dict[station]['StartDate'].split()[0]
-                row[3] = avgs_dict[station]['EndDate'].split()[0]
-                ucursor.updateRow(row)
+#: Save features to .json, load .json as a feature class
+print(f'Saving JSON to {temp_json_path}...')
+with open(temp_json_path, 'w') as json_file:
+    json_file.write(feature_set.JSON)
 
-    #: Add anchor points for the symbology
-    print('Adding anchor points...')
-    anchor_fields = ['DetectorStation', 'AvgChange7D', 'SHAPE@XY']
-    with arcpy.da.InsertCursor(temp_fc_path, anchor_fields) as icursor:
-        null_island = (0,0)
-        icursor.insertRow(['AnchorLow', 24, null_island])
-        icursor.insertRow(['AnchorHigh', 100, null_island])
+print(f'Creating temp feature class {temp_fc_path}...')
+arcpy.JSONToFeatures_conversion(temp_json_path, temp_fc_path)
 
-    #: Overwrite existing AGOL service
-    feature_name = 'TrafficChanges'
-    sddraft_path = os.path.join(temp_dir, f'{feature_name}.sddraft')
-    sd_path = sddraft_path[:-5]
+#: Add our new columns.
+print('Adding columns...')
+columns = [('DetectorStation', 'TEXT'), ('AvgChange7D', 'DOUBLE'), ('StartDate', 'TEXT'), ('EndDate', 'TEXT')]
+for col in columns:
+    name, dtype = col
+    arcpy.AddField_management(temp_fc_path, name, dtype)
 
-    print(f'Connecting to AGOL as {secrets.USERNAME}...')
-    gis = arcgis.gis.GIS('https://www.arcgis.com', secrets.USERNAME, secrets.PASSWORD)
-    sd_item = gis.content.get(secrets.SD_ITEM_ID)
+#: Update the temp feature class with new averages
+print('Updating feature class with new averages...')
+fields = ['DetectorStation', 'AvgChange7D', 'StartDate', 'EndDate']
+with arcpy.da.UpdateCursor(temp_fc_path, fields) as ucursor:
+    for row in ucursor:
+        station = row[0]
+        if station in avgs_dict:
+            row[1] = avgs_dict[station]['AvgChange7D']
+            row[2] = avgs_dict[station]['StartDate'].split()[0]
+            row[3] = avgs_dict[station]['EndDate'].split()[0]
+            ucursor.updateRow(row)
 
-    #: Get project references
-    #: Assume there's only one map in the project, remove all layers for clean map
-    print(f'Getting map from {secrets.PROJECT_PATH}...')
-    project = arcpy.mp.ArcGISProject(secrets.PROJECT_PATH)
-    covid_map = project.listMaps()[0]
-    for layer in covid_map.listLayers():
-        print(f'Removing {layer} from {covid_map.name}...')
-        covid_map.removeLayer(layer)
+#: Add anchor points for the symbology
+print('Adding anchor points...')
+anchor_fields = ['DetectorStation', 'AvgChange7D', 'SHAPE@XY']
+with arcpy.da.InsertCursor(temp_fc_path, anchor_fields) as icursor:
+    null_island = (0,0)
+    icursor.insertRow(['AnchorLow', 24, null_island])
+    icursor.insertRow(['AnchorHigh', 100, null_island])
 
-    layer = covid_map.addDataFromPath(temp_fc_path)
-    project.save()
+#: Overwrite existing AGOL service
+print(f'Connecting to AGOL as {secrets.USERNAME}...')
+gis = arcgis.gis.GIS('https://www.arcgis.com', secrets.USERNAME, secrets.PASSWORD)
+sd_item = gis.content.get(secrets.SD_ITEM_ID)
 
-    #: draft, stage, update, publish
-    print(f'Staging and updating...')
-    sharing_draft = covid_map.getWebLayerSharingDraft('HOSTING_SERVER', 'FEATURE', feature_name, [layer])
-    sharing_draft.exportToSDDraft(sddraft_path)
-    arcpy.server.StageService(sddraft_path, sd_path)
-    sd_item.update(data=sd_path)
-    sd_item.publish(overwrite=True)
+#: Get project references
+#: Assume there's only one map in the project, remove all layers for clean map
+print(f'Getting map from {secrets.PROJECT_PATH}...')
+project = arcpy.mp.ArcGISProject(secrets.PROJECT_PATH)
+covid_map = project.listMaps()[0]
+for layer in covid_map.listLayers():
+    print(f'Removing {layer} from {covid_map.name}...')
+    covid_map.removeLayer(layer)
+
+layer = covid_map.addDataFromPath(temp_fc_path)
+project.save()
+
+#: draft, stage, update, publish
+print(f'Staging and updating...')
+sharing_draft = covid_map.getWebLayerSharingDraft('HOSTING_SERVER', 'FEATURE', feature_name, [layer])
+sharing_draft.exportToSDDraft(sddraft_path)
+arcpy.server.StageService(sddraft_path, sd_path)
+sd_item.update(data=sd_path)
+sd_item.publish(overwrite=True)
+
+#: Cleanup
+to_delete = [sddraft_path, sd_path, temp_json_path, temp_fc_path]
+for item in to_delete:
+    if arcpy.Exists(item):
+        arcpy.Delete_management(item)
